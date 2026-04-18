59141926562dc0810120ff7ea20e7b204862d158
Nirmal Chaudhari <nirmalchaudhari1357@gmail.com>
2026-04-19 02:29:09 +0530

    feat: added delay after player crush or win

--- Assets/Script/CrushDetection.cs
+++ Assets/Script/CrushDetection.cs
@@ -1,15 +1,22 @@
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrushDetection : MonoBehaviour
 {
+    [SerializeField] float RestartDelay = 1f;
     void OnTriggerEnter2D(Collider2D other) 
     {
+        // define's index number for layer = floor
         int layerIndex = LayerMask.NameToLayer("Floor");
 
         if(other.gameObject.layer == layerIndex)
         {
-            Debug.Log("You lose");
+            Invoke("ReloadScene", RestartDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Script/FinishLine.cs
+++ Assets/Script/FinishLine.cs
@@ -1,14 +1,23 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float RestartDelay = 1f;
     void OnTriggerEnter2D(Collider2D other) 
     {
+        //define's index number for layer = Player
         int layerIndex = LayerMask.NameToLayer("Player");
 
         if (other.gameObject.layer == layerIndex)
         {
-            Debug.Log("You won!!");
+            Invoke("ReloadScene", RestartDelay);
         }
+
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
